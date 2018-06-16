9cfcaa32500956206a2bd990b4df56afc09f072c
danielboso <bosodaniel@hotmail.com>
2018-06-16 18:04:05 -0300

    Adição de comentários referentes sobre os requisitos a serem implementados

---
+++ trabalho_final.sol
@@ -0,0 +1,24 @@
+
+/// ------------------------------------------------------
+///   Ancoramento Temporal
+/// ------------------------------------------------------
+
+/// Medição do tempo decorrido entre duas transações
+
+
+/// Tempo Atual
+
+
+/// Medição do tempo decorrido desde uma dada transação
+
+/// ------------------------------------------------------
+///   Controle de estacionamento
+/// ------------------------------------------------------
+
+///   Registro da entrada e saída de veículos
+
+
+///   Medição do tempo de estadia do veículo no estacionamento
+
+
+///   Cobrança extra por exeder o horário limite
