30f187e16e4f40b193964b9af59f9802c3a6a4ca
danielboso <bosodaniel@hotmail.com>
2018-06-17 22:02:45 -0300

    refactoring

--- trabalho_final.sol
+++ trabalho_final.sol
@@ -56,17 +56,19 @@ contract Estacionamento {
   mapping(address => Cliente) public clients;
   uint priceForMinute;
 
+  // Constructor for the parking
   constructor(uint price) public {
     parking = msg.sender;
     priceForMinute = price;
   }
 
-  // Registro do horário de entrada do veículo
+  // Register of the time that vehicle entry
   function registerClient(address client) public {
     if (msg.sender != parking) return;
     clients[client].beginTimeParked = now;
   }
 
+  // Register of the time that vehicle exited
   function registerExitClient(address client) public {
     if (msg.sender != parking) return;
     clients[client].finalTimeParked = now;
@@ -82,11 +84,9 @@ contract Estacionamento {
 contract Registro {
 
     struct car {
-
         bool parked;
         uint time_in;
         uint time_out;
-
     }
 
     uint constant MAX = 6;
@@ -100,7 +100,6 @@ contract Registro {
     event ExtraPayed(address car);
 
     constructor() public {
-
         controller = msg.sender;
     }
 
@@ -110,7 +109,6 @@ contract Registro {
         parking[msg.sender].time_in = now;
         emit Parked(msg.sender);
     }
-
 
     function leaveParking() public {
         if ( msg.sender == controller || parking[msg.sender].parked == false) return;
@@ -122,12 +120,8 @@ contract Registro {
             parking[msg.sender].time_out = leavingTime;
             emit Leave(msg.sender);
         }else{
-
             extras[msg.sender] = new Extra(msg.sender);
-
-
         }
-
     }
 
     function payExtra(uint amount)public {
@@ -143,40 +137,31 @@ contract Registro {
             parking[msg.sender].time_out = leavingTime;
             emit ExtraPayed(msg.sender);
         }
-
     }
-
 }
 
-
 ///   Medição do tempo de estadia do veículo no estacionamento
-
 
 ///   Cobrança extra por exeder o horário limite
 
 contract Extra {
 
-    uint constant AMOUNT = 6 * now;
+    uint256 constant AMOUNT = 6 * now;
 
-    address public owner;
+    address public _owner;
     mapping (address => uint) public balances;
 
     event Sent(address from, address to, uint amount);
 
     constructor(address owner) public {
-
-        owner = owner;
+        _owner = owner;
     }
 
     function send(address receiver, uint amount) public returns (bool){
-
-        if(msg.sender != owner || amount < AMOUNT || balances[msg.sender] < amount) return false;
-
+        if(msg.sender != _owner || amount < AMOUNT || balances[msg.sender] < amount) return false;
         balances[msg.sender] -= amount;
         balances[receiver] += amount;
         emit Sent(msg.sender, receiver, amount);
         return true;
     }
-
-
 }
